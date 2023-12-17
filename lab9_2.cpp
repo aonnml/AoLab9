7ccce1a0169c2cfcf330252bfd4dea80a7167bfa
aonnml <radchanon694@gmail.com>
2023-12-17 12:27:49 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -4,6 +4,19 @@ using namespace std;
 //Write the function printO() here
 
 int main(){
+
+void printO(int N,int M){
+    if(N <= 0 || M <= 0){
+    cout << "Invalid input";
+    }else{
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                cout << 'O';
+        }
+    cout << '\n';
+        }
+    }
+}
 	
 	printO(2,2);
 	cout << "\n";
